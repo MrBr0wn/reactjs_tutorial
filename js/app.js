c7f256c9eee9eb48062f85f3b327296ac34f73e3
Mr. Brown <shamil52.93@gmail.com>
2017-12-19 01:06:44 +0500

    if-else operators, small refactoring

--- js/app.js
+++ js/app.js
@@ -12,22 +12,43 @@ var my_news = [
     text: 'Text_3'
   }
 ];
+my_news = [];
+
+var Article = React.createClass({
+  render: function() {
+    var author = this.props.data.author,
+        text = this.props.data.text;
+
+    return (
+      <div className = "article">
+        <p className = "news_author">{author}:</p>
+        <p className = "news_text">{text}</p>
+      </div>
+    );
+  }
+});
 
 var News = React.createClass({
   render: function() {
     var data = this.props.data;
-    var newsTemplate = data.map(function(item, index) {
-      return (
-        <div key = {index}>
-          <p className = "news_author">{item.author}:</p>
-          <p className = "news_text">{item.text}</p>
-        </div>
-      );
-    })
+    var newsTemplate;
 
+    if(data.length > 0) {
+      newsTemplate = data.map(function(item, index) {
+        return (
+          <div key = {index}>
+            <Article data = {item} />
+          </div>
+        )
+      })
+    }
+    else {
+      newsTemplate = <p>Sorry...No news ;(</p>
+    }
     return (
       <div className = "news">
         {newsTemplate}
+        <strong className = {'news_count ' + (data.length > 0 ? ' ' : 'none') }>Count news: {data.length}</strong>
       </div>
     );
   }
@@ -37,22 +58,8 @@ var App = React.createClass({
   render: function() {
     return (
       <div className = "app">
-        q all! Im component App and i can display news!
-
+        <h3>NEWS</h3>
         <News data={my_news} />
-
-        <Comments />
-
-      </div>
-    );
-  }
-});
-
-var Comments = React.createClass({
-  render: function() {
-    return (
-      <div className = "comments">
-        No comments...
       </div>
     );
   }
